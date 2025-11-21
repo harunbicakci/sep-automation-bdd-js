246f348be75ef90376fc441cb382607297925b63
Mad Triathlete <h_bicakci@msn.com>
2025-11-20 21:19:44 -0500

    sep19-click next button step1 is complete

--- steps/getting-started/clickNextButtonStep1.js
+++ steps/getting-started/clickNextButtonStep1.js
@@ -1,5 +1,15 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
 
+When(
+  "user has completed step one with only required fields",
+  async function () {
+    await startApplicationPage.enterFirstName("TestName");
+    await startApplicationPage.enterLastName("TestLastName");
+    await startApplicationPage.enterEmail("test@test.com");
+    await startApplicationPage.enterPhoneNumber("4079876543");
+    await startApplicationPage.clickNextButton();
+  }
+);
